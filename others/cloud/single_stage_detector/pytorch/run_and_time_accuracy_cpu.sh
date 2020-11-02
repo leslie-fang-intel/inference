1734b9189d15a8449269856f80dacaf895247e49
XiaobingSuper <xiaobing.zhang@intel.com>
2020-11-02 17:14:05 +0800

    updata int8 json file

--- others/cloud/single_stage_detector/pytorch/run_and_time_accuracy_cpu.sh
+++ others/cloud/single_stage_detector/pytorch/run_and_time_accuracy_cpu.sh
@@ -2,6 +2,7 @@
 
 ###############################################################################
 ### How to run?
+### option step(for int8 calibration): bash run_and_time_accuracy_cpu.sh int8 jit ssd_resnet34.json calibration 3
 ### 1) int8 inference accuracy
 ###    bash run_and_time_accuracy_cpu.sh int8 jit ssd_resnet34.json
 ### 2) fp32 infenence accuracy
@@ -11,6 +12,8 @@
 
 export DNNL_PRIMITIVE_CACHE_CAPACITY=1024
 export USE_IPEX=1
+
+BATCH_SIZE=32
 
 DATASET_DIR='/lustre/dataset/COCO2017'
 CHECKPOINT='../pretrained/resnet34-ssd1200.pth'
@@ -32,4 +35,13 @@ else
     echo "### running native path"
 fi
 
-python infer.py --seed 1 --threshold 0.2 --data ${DATASET_DIR} --device 0 --checkpoint $CHECKPOINT --no-cuda --ipex $ARGS $CONFIG_FILE
+if [ "$4" == "calibration"  ]; then
+    ARGS="$ARGS --calibration"
+    echo "### running int8 calibration"
+fi
+
+if [ -n "$5" ]; then
+    ARGS="$ARGS --iter $5"
+fi
+
+python infer.py --seed 1 --threshold 0.2 -b $BATCH_SIZE -j 0 --data ${DATASET_DIR} --device 0 --checkpoint $CHECKPOINT --no-cuda --ipex $ARGS $CONFIG_FILE
